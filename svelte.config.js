3972a96c23f0a8293728b14d84e9d182667bce65
Thomas Scheepers <60781257+Knetters@users.noreply.github.com>
2024-01-25 13:54:20 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+	  // Set your other SvelteKit configuration options here
+	},
+	adapter: adapter(),
 };
